54209ad5760989c052dbf054b88889758c715ec5
Alex2021cyberhuman <Alex2021cyberhuman@gmail.com>
2023-03-20 18:37:45 +0500

    Removed fft

--- optimizations/JPEG.Solved/Program.cs
+++ optimizations/JPEG.Solved/Program.cs
@@ -6,10 +6,9 @@ using JPEG.Solved.Processor;
 
 Console.WriteLine(nint.Size == 8 ? "64-bit version" : "32-bit version");
 Console.WriteLine($"Processors: {ProgramConstants.ProcessorCount}");
-// var processor = BrokenBoostedJpegProcessor.Init;
 var processor = BoostedJpegProcessor.Init;
 var sw = Stopwatch.StartNew();
-var imagePath = args.ElementAtOrDefault(0) ??  @"sample.bmp";
+var imagePath = args.ElementAtOrDefault(0) ?? @"sample.bmp";
 // var imageName = "Big_Black_River_Railroad_Bridge.bmp";
 var compressedImagePath = imagePath +
                           ".compressed." +
@@ -18,18 +17,16 @@ var uncompressedImagePath = imagePath +
                             ".uncompressed." +
                             BoostedJpegProcessor.CompressionQuality +
                             ".bmp";
-// for (var i = 0; i < 100; i++)
-// {
-    sw.Restart();
-    processor.Compress(imagePath, compressedImagePath);
-    sw.Stop();
-    Console.WriteLine("Compression: " + sw.ElapsedMilliseconds);
-    
-    sw.Restart();
-    processor.Uncompress(compressedImagePath, uncompressedImagePath);
-    sw.Stop();
-    Console.WriteLine("Decompression: " + sw.ElapsedMilliseconds);
-// }
+
+sw.Restart();
+processor.Compress(imagePath, compressedImagePath);
+sw.Stop();
+Console.WriteLine("Compression: " + sw.ElapsedMilliseconds);
+
+sw.Restart();
+processor.Uncompress(compressedImagePath, uncompressedImagePath);
+sw.Stop();
+Console.WriteLine("Decompression: " + sw.ElapsedMilliseconds);
 Console.WriteLine(
     $"Peak commit size: {MemoryMeter.PeakPrivateBytes() / (1024.0 * 1024):F2} MB");
 Console.WriteLine(
